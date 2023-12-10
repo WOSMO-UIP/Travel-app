bcd0c3511ec2fdff2f02ad02eb503a2dabe32a8e
Wosmos <m.wasifmalik17@gmail.com>
2023-12-10 20:30:46 +0500

    code pushed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
                     </form>
                 </div>
             </div>
-
+          
             <div className='flex flex-col max-w-[1400px] px-10 py-4 mx-auto justify-between sm:flex-row text-center text-purple-300 '>
                 <p className='py-4 text-purple-300 '>2022 Experiences, LLC. All rights reserved</p>
                 <div className='flex justify-between sm:w-[300px] pt-4 text-2xl text-purple-300 '>
